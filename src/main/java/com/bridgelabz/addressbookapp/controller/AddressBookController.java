c009e15d67e497cc658d3530e47f035266cc6bc7
AmolDhait <amol.dhait97@gmail.com>
2022-08-27 18:08:35 +0530

    Address Book App Setup Create

---
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbookapp.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/addressbookservice")
+public class AddressBookController {
+
+}
